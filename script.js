6c121f715af12c58e2fedd74556f7ae8e46992e9
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2023-08-12 23:34:44 +0530

    add range picker functionality

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ const rainbow_btn = document.getElementById("rainbow_btn");
 const grad_btn = document.getElementById("grad_btn");
 const eraser_btn = document.getElementById("eraser_btn");
 const clear_btn = document.getElementById("clear_btn");
+const range_picker = document.getElementById("range_picker");
 const leftbutttons = document.querySelectorAll(".left_menu .toggle_btn");
 const root = document.querySelector(":root");
+const grid_size_display = document.querySelector("#range_selector p");
 let temp;
 const eraser_var = getComputedStyle(root).getPropertyValue("--text-clr");
 let active_button = color_btn;
 let color_var = color_picker.value;
 let grad_var;
 let grad_numvar = [];
-
-for (let index = 0; index < 1024; index++) {
-  var newDiv = document.createElement("div");
-  canvas.appendChild(newDiv);
-}
-const grid = document.querySelectorAll(".canvas div");
+let grid_size = parseInt(range_picker.value);
 
 // Utility Functions
 function GenRandomClr() {
@@ -33,11 +30,34 @@ function GradFunction(element) {
   grad_var = (grad_var.slice(4).slice(0, -1)).split(", ");
   grad_numvar = [];
   grad_var.forEach((element) => {
-    grad_numvar.push(parseFloat(element) - (0.1 * parseFloat(element)));
+    grad_numvar.push(parseFloat(element) - (0.2 * parseFloat(element)));
   });
   return ("rgb(" + grad_numvar[0] + ", " + grad_numvar[1] + ", " +
     grad_numvar[2] + ")");
 }
+function createGrid(grid_size) {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+  for (let index = 0; index < grid_size * grid_size; index++) {
+    let newDiv = document.createElement("div");
+    canvas.appendChild(newDiv);
+  }
+  canvas.style.setProperty(
+    "grid-template-columns",
+    `repeat(${grid_size}, 1fr)`,
+  );
+  grid = document.querySelectorAll(".canvas div");
+  grid_size_display.textContent = `Grid size: ${grid_size} x ${grid_size}`;
+
+  grid.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      main_function(element, active_button.id);
+      // Do Something
+    });
+  });
+}
+createGrid(grid_size);
 
 // Main Function
 function main_function(element, active_button) {
@@ -53,12 +73,6 @@ function main_function(element, active_button) {
 }
 
 // Event Listeners
-grid.forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    main_function(element, active_button.id);
-    // Do Something
-  });
-});
 
 leftbutttons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -76,4 +90,9 @@ clear_btn.addEventListener("click", () => {
 
 color_picker.addEventListener("input", () => {
   color_var = color_picker.value;
+});
+range_picker.addEventListener("input", () => {
+  grid_size = parseInt(range_picker.value);
+  console.log(grid_size);
+  createGrid(grid_size);
 });
